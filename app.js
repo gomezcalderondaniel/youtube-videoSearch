3e62d6753285420b7c98799aef48f17143f26065
gomezcalderondaniel <42280710+gomezcalderondaniel@users.noreply.github.com>
2018-12-03 14:48:01 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+function tplawesome(e, t) { res = e; for (var n = 0; n < t.length; n++) { res = res.replace(/\{\{(.*?)\}\}/g, function (e, r) { return t[n][r] }) } return res }
+
+$(function() {
+    $("form").on("submit", function(e) {
+       e.preventDefault();
+       // prepare the request
+       var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 3,
+            order: "viewCount",
+            publishedAfter: "2017-01-01T00:00:00Z"
+       }); 
+       // execute the request
+       request.execute(function(response) {
+          var results = response.result;
+          $("#vidResults").html("");
+          $.each(results.items, function(index, item) {
+            $.get("item.html", function(data) {
+                $("#vidResults").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+            });
+          });
+          resetVideoHeight();
+       });
+    });
+    
+    $(window).on("resize", resetVideoHeight);
+});
+
+function resetVideoHeight() {
+    $(".video").css("height", $("#vidResults").width() * 9/16);
+}
+
+function init() {
+    gapi.client.setApiKey("AIzaSyA9kFe0UNEDwyN0XRhozQf-78VIB5qjD7g");
+    gapi.client.load("youtube", "v3", function() {
+        // yt api is ready
+    });
+}
